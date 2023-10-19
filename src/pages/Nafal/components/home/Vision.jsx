59f7208bf7083e2fea97636c3e7b874dfeed9967
Mohammad Adnan <mdadnanengg@gmail.com>
2023-10-19 07:26:58 +0530

    vision section aligned

--- src/pages/Nafal/components/home/Vision.jsx
+++ src/pages/Nafal/components/home/Vision.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import video from '../../../../assets/videos/nafal_vision.mp4'
 import { Col, Row, Container } from 'reactstrap'
-function Vision() {
+const Vision = () => {
     return (
-        <div className="h-auto d-flex justify-content-center">
-            <div className='title'>
-                <h3>Vision</h3>
-            </div>
-            <div className="h-auto shadow-lg  p-3 mb-5 bg-body rounded">
-                <video width="750" height="500" controls  >
-                    <source src={video} type="video/mp4" />
-                </video>
-            </div>
+      <div className="d-flex flex-column align-items-center justify-content-center">
+        <div className="title">
+          <h3>Vision</h3>
         </div>
-    )
+        <div className="shadow-lg p-3 mb-5 bg-body rounded">
+          <video width="750" height="500" controls>
+            <source src={video} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+    );
 }
 
 export default Vision
