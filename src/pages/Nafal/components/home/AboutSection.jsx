d2233c7100606fca8e7250cf41d2a6e84e08f927
Mohammad Adnan <mdadnanengg@gmail.com>
2023-10-20 02:53:06 +0530

    carousel about vision section improved

--- src/pages/Nafal/components/home/AboutSection.jsx
+++ src/pages/Nafal/components/home/AboutSection.jsx
@@ -1,63 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Container, Row, Col } from 'reactstrap';
-import mockData from '../../mock/mockAboutData.json';
-import about from "../../../../assets/images/course/about.jpg";
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
 
-const AboutSection = () => {
+//import images
+import about from "../../../../assets/images/coworking/about.jpg";
+
+function AboutSection() {
   return (
-    <React.Fragment>
-      <section className="section pt-0">
-        <Container>
-          <Row className="align-items-center">
-            <Col lg="5" md="6" xs="12">
-              <img
-                src={about}
-                className="img-fluid shadow rounded"
-                alt="Image not found!!"
-              />
-            </Col>
-            <Col lg="7" md="6" xs="12" className="mt-4 mt-sm-0 pt-2 pt-sm-0">
-              <div className="section-title ms-lg-4">
-                <h4 className="title mb-4">
-                  <span className="text-primary">{mockData.data.title}</span>
-                </h4>
-                <p className="text-muted">{mockData.data.discription}</p>
-                {/* <ul className="list-unstyled feature-list mb-0 text-muted">
-                  <li>
-                    <i className="uil uil-arrow-circle-right text-primary h5 mb-0 align-middle me-2"></i>
-                    Digital Marketing Solutions for Tomorrow
-                  </li>
-                  <li>
-                    <i className="uil uil-arrow-circle-right text-primary h5 mb-0 align-middle me-2"></i>
-                    Our Talented & Experienced Marketing Agency
-                  </li>
-                  <li>
-                    <i className="uil uil-arrow-circle-right text-primary h5 mb-0 align-middle me-2"></i>
-                    Create your own skin to match your brand
-                  </li>
-                </ul> */}
-                <Container>
-                  <Row>
-                    <Col xs="12" sm="12"  md="4" lg="4" xl="4">
-                      <Link to="#" className="btn btn-outline-primary mt-3">
-                        Company Profile
-                      </Link>
-                    </Col>
-                    <Col xs="12" sm="12" md="4" lg="4" xl="4">
-                      <Link to="#" className="btn btn-outline-primary mt-3">
-                        Read More...
-                      </Link>
-                    </Col>
-                  </Row>
-                </Container>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </section>
-    </React.Fragment>
+    <section className="section">
+      <Container>
+        <Row className="align-items-center">
+          <Col lg="5" md="6" xs="12">
+            <img src={about} className="img-fluid rounded" alt="" />
+          </Col>
+
+          <Col lg="7" md="6" xs="12" className="mt-4 mt-sm-0 pt-2 pt-sm-0">
+            <div className="section-title ms-lg-4">
+              <h4 className="title mb-4">About Us</h4>
+              <p className="text-muted">
+                Experience the best in HVAC services with{" "}
+                <span className="text-primary fw-bold">Your HVAC Company</span>.
+                We provide top-notch heating, cooling, and ventilation solutions
+                to ensure your home's comfort and efficiency. Our team of experts
+                is committed to delivering outstanding service and reliability.
+              </p>
+              <Link to="#" className="btn btn-primary mt-3">
+                Read more
+              </Link>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   );
-};
+}
 
 export default AboutSection;
--- src/pages/Nafal/components/home/Carousel.jsx
+++ src/pages/Nafal/components/home/Carousel.jsx
@@ -1,51 +1,141 @@
-import React from 'react';
-import { Col, Container, Row, UncontrolledCarousel } from 'reactstrap';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Container, Row, Col } from "reactstrap";
+import FeatherIcon from "feather-icons-react";
+import Slider from "react-slick";
+import ModalVideo from "react-modal-video";
+import "react-modal-video/scss/modal-video.scss";
 
-//import Images
-//import Images
-import img01 from '../../../../assets/images/real/1.jpg';
-import img02 from '../../../../assets/images/real/2.jpg';
-import img03 from '../../../../assets/images/real/3.jpg';
+import bg01 from "../../../../assets/images/blog/bg1.jpg";
+import bg02 from "../../../../assets/images/blog/bg2.jpg";
+import bg03 from "../../../../assets/images/blog/bg3.jpg";
+import bg04 from "../../../../assets/images/blog/bg3.jpg";
 
 const Carousel = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const items = [
+    {
+      id: 1,
+      image: bg01,
+      h1: "Experience Comfort All Year Round",
+      p:
+        "Transform your home's climate with our expert HVAC services. We specialize in providing efficient heating, cooling, and ventilation solutions for your comfort.",
+      btnclass: "btn btn-icon btn-pills btn-primary lightbox",
+      link: "#contact-us",
+      isVideo: true,
+    },
+    {
+      id: 2,
+      image: bg02,
+      h1: "Meet Our Team of HVAC Experts",
+      p:
+        "Our dedicated team of professionals is ready to serve you. Learn more about our experienced technicians, our commitment to quality, and our passion for exceptional HVAC services.",
+      btnclass: "btn btn-primary",
+      btntext: "About Us",
+      link: "/about-us",
+      iClass: "mdi mdi-tools",
+      isVideo: false,
+    },
+    {
+      id: 3,
+      image: bg03,
+      h1: "Meet Our Team of HVAC Experts",
+      p:
+        "Our dedicated team of professionals is ready to serve you. Learn more about our experienced technicians, our commitment to quality, and our passion for exceptional HVAC services.",
+      btnclass: "btn btn-primary mouse-down",
+      btntext: "Contact Us",
+      link: "#contact-us",
+      iClass: "mdi mdi-home-thermostat",
+      isVideo: false,
+    }
+  ];
+  
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  useEffect(() => {
+    var e1 = document.getElementsByClassName("slick-slide");
+    for (var i = 0; i < 3; i++) {
+      if (i === 0) e1[i].style.backgroundImage = `url(${bg01})`;
+      if (i === 1) e1[i].style.backgroundImage = `url(${bg02})`;
+      if (i === 2) e1[i].style.backgroundImage = `url(${bg03})`;
+    }
+
+    document.getElementById("btn1").addEventListener("click", openModal);
+
+    return () => {
+      document.getElementById("btn1").removeEventListener("click", openModal);
+    };
+  }, []);
+
+  var settings = {
+    autoplay: true,
+    infinite: true,
+    autoplaySpeed: 1500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    draggable: true,
+    pauseOnHover: true,
+  };
+
+  const slides = items.map((item, key) => (
+    <li
+      className="bg-home bg-animation-left d-flex align-items-center"
+      key={key}
+      style={{ backgroundImage: `url(${item.image})` }}
+    >
+      <Container>
+        <Row className="align-items-center">
+          <Col lg={7} md={7} className="slider-desc">
+            <div
+              className="title-heading position-relative mt-4"
+              style={{ zIndex: "1" }}
+            >
+              <h1
+                className="heading mb-3"
+                dangerouslySetInnerHTML={{ __html: item.h1 }}
+              ></h1>
+              <p className="para-desc">{item.p}</p>
+              <div className="mt-4 pt-2">
+                <Link to={item.link} id={"btn" + item.id} className={item.btnclass}>
+                  {item.isVideo ? (
+                    <FeatherIcon icon="video" className="icons" />
+                  ) : (
+                    <i className={item.iClass}></i>
+                  )}
+                  {item.btntext}
+                </Link>
+                {item.isVideo && (
+                  <span className="fw-bold text-uppercase small align-middle ms-2">
+                    Watch Now
+                  </span>
+                )}
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </li>
+  ));
+
   return (
     <React.Fragment>
-      <div className="page-content">
-        <Container fluid>
-          <Row>
-            <Col xs="12">
-              <section className="home-slider position-relative carousel-overlay">
-                <div>
-                  <div className="bg-overlay"></div>
-                  <UncontrolledCarousel
-                    interval={4000}
-                    items={[
-                      {
-                        altText: ' ',
-                        caption: ' ',
-                        key: 1,
-                        src: img01,
-                      },
-                      {
-                        altText: ' ',
-                        caption: ' ',
-                        key: 2,
-                        src: img02,
-                      },
-                      {
-                        altText: ' ',
-                        caption: ' ',
-                        key: 3,
-                        src: img03,
-                      },
-                    ]}
-                  />
-                </div>
-              </section>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+      <section className="main-slider">
+        <Slider className="slides" {...settings}>
+          {slides}
+        </Slider>
+      </section>
+
+      <ModalVideo
+        channel="youtube"
+        isOpen={isOpen}
+        videoId="yba7hPeTSjk"
+        onClose={() => setIsOpen(false)}
+      />
     </React.Fragment>
   );
 };
