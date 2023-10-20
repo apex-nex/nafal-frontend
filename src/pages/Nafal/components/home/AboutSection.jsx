fffd486974ff49eb9e2d2540e809c42c447574f3
AbdulRaheman143 <abdul.rehman@vernost.in>
2023-10-21 01:37:52 +0530

    changes added

--- src/pages/Nafal/components/home/AboutSection.jsx
+++ src/pages/Nafal/components/home/AboutSection.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Row, Col } from 'reactstrap';
-import about from '../../../../assets/images/course/about.jpg';
 
 //import images
 import about from "../../../../assets/images/coworking/about.jpg";
