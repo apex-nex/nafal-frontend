9840b183a3a49db7ead843666ae04812e849df75
AbdulRaheman143 <abdul.rehman@vernost.in>
2023-10-21 01:34:01 +0530

    changes done

--- src/pages/Nafal/components/home/AboutSection.jsx
+++ src/pages/Nafal/components/home/AboutSection.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Row, Col } from 'reactstrap';
-import mockData from '../../mock/mockHomePageData.json';
 import about from '../../../../assets/images/course/about.jpg';
 
 //import images
