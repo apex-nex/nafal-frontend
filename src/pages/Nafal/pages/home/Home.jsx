a9d80400794dd5c7a8e93fbf23339586318b1788
Mohammad Adnan <mdadnanengg@gmail.com>
2023-10-21 00:27:07 +0530

    add meta-tag in homepage

--- src/pages/Nafal/pages/home/Home.jsx
+++ src/pages/Nafal/pages/home/Home.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import Carousel from '../../components/home/Carousel'
-import AboutSection from '../../components/home/AboutSection'
-import Vision from '../../components/home/Vision'
-
+import React from 'react';
+import MetaTags from 'react-meta-tags';
+import Carousel from '../../components/home/Carousel';
+import AboutSection from '../../components/home/AboutSection';
+import Vision from '../../components/home/Vision';
 
 const Home = () => {
   return (
     <React.Fragment>
-      <div className='carousel'>
-      <Carousel/>
-      </div>
-      <div className='about-section'>
-      <AboutSection/>
-      </div>
-      <div className='vision-container mb-5'>
-      <Vision/>
+      <div className="page-content">
+        <MetaTags>
+          <title>Home | Nafal</title>
+        </MetaTags>
+        <div className="carousel">
+          <Carousel />
+        </div>
+        <div className="about-section">
+          <AboutSection />
+        </div>
+        <div className="vision-container mb-5">
+          <Vision />
+        </div>
       </div>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
