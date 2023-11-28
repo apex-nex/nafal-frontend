a7d043e76656b61596169b1213515695b8d367cc
AbdulRaheman143 <abdul.rehman@vernost.in>
2023-11-29 00:29:37 +0530

    about page changes added

--- src/pages/Nafal/components/about/KeyFeatureBoxNafal.js
+++ src/pages/Nafal/components/about/KeyFeatureBoxNafal.js
@@ -4,10 +4,10 @@ import { Col } from 'reactstrap';
 import FeatherIcon from 'feather-icons-react';
 
 export default function KeyFeatureBox(props) {
-  const { keyfeatures } = props;
+  const { keyfeatures ,isSeeMore} = props;
   return (
     <React.Fragment>
-      {keyfeatures.map((feature, key) => (
+      {keyfeatures.slice(0,isSeeMore ?keyfeatures.length :9 ).map((feature, key) => (
         <Col lg="4" md="6" key={key} className="mt-4 pt-2" name="keyfeature">
           <div className="d-flex key-feature align-items-center p-3 rounded shadow">
             <div className="icon text-center rounded-circle me-3">
