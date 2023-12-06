791dedcadac29544189d1ddb496bf069ed7dccb5
Mohammad Adnan <mdadnanengg@gmail.com>
2023-12-07 02:21:15 +0530

    clean up semi finished

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,7 +2,7 @@ import React, { Component, Suspense } from "react";
 import { withRouter } from "react-router-dom";
 import BackToTop from "./backToTop";
 import NavBarNafal from '../../pages/Nafal/components/common/header/navbar/Navbar'
-import FooterNafal from '../../pages/Nafal/components/common/footer/Footer'
+import FooterNafal from '../footer/Footer'
 
 function Layout(props) {
   return (
